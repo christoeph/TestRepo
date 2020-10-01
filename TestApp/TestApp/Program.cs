ff80942b893f3e97e6b3707f6d70a375f73cfd74
ru2069 <71378953+ru2069@users.noreply.github.com>
2020-10-01 16:18:55 +0200

    Update Program.cs

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -8,7 +8,27 @@ namespace TestApp
         {
             Console.WriteLine("Hello");
             Console.WriteLine(":-)");
+            
+            double a;
+            Boolean weiter = true;
 
+            while (weiter)
+            {
+                Console.WriteLine("Bitte 1 eingeben");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                if (a == 1)
+                {
+                    Console.WriteLine("Korrekte Eingabe");
+                }
+                else
+                {
+                    Console.WriteLine("Fehler: Eingabe");
+                }
+
+                Console.WriteLine("Weiter mit 'w': ");
+                weiter = Console.ReadLine().Equals("w");
+            }
             Console.ReadKey();
         }
     }
