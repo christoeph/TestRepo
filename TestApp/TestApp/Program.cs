e6d0aa41118873312a322ab0b13d4c28008713ca
luccajade <lucca12jade@users.noreply.github.com>
2020-10-01 16:04:55 +0200

    Update Program.cs

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -7,6 +7,8 @@ namespace TestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine(":-)");
+
             Console.ReadKey();
         }
     }
